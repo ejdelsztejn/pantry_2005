685eacd66fa6eb1a3963b157662bee4365c0a5b6
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-03 12:27:56 -0400

    Add test it can restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/ingredient'
 require './lib/pantry'
 
 class PantryTest < MiniTest::Test
@@ -13,5 +14,22 @@ class PantryTest < MiniTest::Test
     pantry = Pantry.new
 
     assert_equal ({}), pantry.stock
+  end
+
+  def test_it_can_restock
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+
+    assert_equal 15, pantry.stock_check(ingredient1)
+
+    pantry.restock(ingredient2, 7)
+
+    pantry.stock_check(ingredient2)
   end
 end
