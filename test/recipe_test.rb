6eacf46eff685a8b2a294f4d568b330d817a6590
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-03 12:31:13 -0400

    Add recipe test it exists

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -3,5 +3,9 @@ require 'minitest/pride'
 require './lib/recipe'
 
 class RecipeTest < MiniTest::Test
-  
+  def test_it_exists
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_instance_of Recipe, recipe1
+  end
 end
