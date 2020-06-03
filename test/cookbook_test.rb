463ffd6f8fd0eedfe98320cce79c1b939e260958
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-03 13:07:35 -0400

    Add cookbook test it can return ingredients

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/ingredient'
 require './lib/recipe'
 require './lib/cook_book'
 
@@ -27,5 +28,25 @@ class CookBookTest < MiniTest::Test
     cookbook.add_recipe(recipe2)
 
     assert_equal [recipe1, recipe2], cookbook.recipes
+  end
+
+  def test_it_can_return_ingredients
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], cookbook.ingredients
   end
 end
