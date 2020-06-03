1ac502e106a262f59b59195403594bbdab86641f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-03 13:13:25 -0400

    Add ingredients attribute to cookbook and add ingredient names in add recipe method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,10 +1,15 @@
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes,
+              :ingredients
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+    recipe.ingredients.each do |ingredient|
+      @ingredients << ingredient.name if ingredients.include?(ingredient.name) == false
+    end
   end
 end
