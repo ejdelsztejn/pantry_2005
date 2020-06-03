829f9679d448d819d4b03ce5157bd511740415d4
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-03 13:04:07 -0400

    Add addl assertions to total calories test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -45,5 +45,15 @@ class RecipeTest < MiniTest::Test
     recipe1.add_ingredient(ingredient2, 8)
 
     assert_equal 440, recipe1.total_calories
+
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    assert_equal 675, recipe2.total_calories
   end
 end
