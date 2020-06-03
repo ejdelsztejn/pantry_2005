2053df099370fd82003522387048c100f47b78e8
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-03 13:50:23 -0400

    Add initial test for cookbook summary (needs a lot of work)

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -75,9 +75,27 @@ class CookBookTest < MiniTest::Test
   def test_it_can_return_date_created
     skip
     cookbook = CookBook.new
-    require "pry"; binding.pry
-    assert_equal Time.stubs(:now).returns(cookbook.date), cookbook.date
+  #   assert_equal Time.stubs(:now.strftime("%m-%d-%Y").returns(cookbook.date), cookbook.date
   end
 
-  
+  def test_it_can_return_cookbook_summary
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_instance_of Array, cookbook.summary
+    
+  end
 end
